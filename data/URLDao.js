58536b8863f34b538b9df530afa3eaf625234739
sakumonk <67976136+sakumonk@users.noreply.github.com>
2022-02-07 22:31:02 -0500

    mechanism for detecting repeat keys

--- data/URLDao.js
+++ data/URLDao.js
@@ -4,9 +4,15 @@ const URL = require("../model/URL");
 
 class URLDao {
 
-  async create({ url, short }) {
-      const url1 = await URL.create({ url, short });
-    return url1;
+  async create({ url, short1 }) {
+    const url2 = await URL.findOne({ short: short1}).exec();
+    if (url2 != null) { //already exist
+      return [];
+    } else {
+      const url1 = await URL.create({ url, short1 });
+      return url1;
+    }
+    
   }
 
 
